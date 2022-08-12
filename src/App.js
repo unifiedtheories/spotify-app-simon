d2eab1f181685c622d779ceee84771b6b4452d9b
Simon McNeely <70974411+unifiedtheories@users.noreply.github.com>
2022-08-12 10:55:02 -0700

    initialize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavigationBar from "./components/NavigationBar";
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
